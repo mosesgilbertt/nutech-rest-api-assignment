8e6ff72f54446f35ba4f5c5a20ec1e0d29dcf668
Moses Simanungkalit <mosesgilbert4@gmail.com>
2025-07-24 16:25:49 +0700

    feat:setup-project-5

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// app.use(router);
-// app.use(errorHandler);
-console.log(process.env);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", require("./routes/index"));
 
 module.exports = app;
