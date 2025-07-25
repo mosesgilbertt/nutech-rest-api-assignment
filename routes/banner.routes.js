ab7ee26cb07d9b3f33861a6c8ce85c15c3729a40
Moses Simanungkalit <80898256+mosesgilbertt@users.noreply.github.com>
2025-07-25 19:37:42 +0700

    Revert "Development"

--- routes/banner.routes.js
+++
@@ -1,7 +0,0 @@
-const BannerController = require("../controllers/banner.controller");
-
-const banner = require("express").Router();
-
-banner.get("/", BannerController.getAllBanner);
-
-module.exports = banner;
--- routes/service.routes.js
+++
@@ -1,8 +0,0 @@
-const ServiceController = require("../controllers/service.controller");
-const authentication = require("../middlewares/authentication");
-
-const service = require("express").Router();
-
-service.get("/", authentication, ServiceController.getAllServices);
-
-module.exports = service;
